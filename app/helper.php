216aba4a2c2356e2820ff05c96d146f513ecdf03
dharmik <dharmikd@zignuts.com>
2023-03-20 18:38:23 +0530

    changes into helper function

--- app/helper.php
+++ app/helper.php
@@ -13,46 +13,6 @@ if(!function_exists('success')){
     }
 }
 
-if(!function_exists('ErrorResponse')){
-    function ErrorResponse($message = null, $data = [], $type = null)
-    {
-        $status = 500;
 
-        switch ($type) {
-            case 'validation':
-                $status  = 422;
-                $message ?? 'Validation Failed please check the request attributes and try again';
-            break;
-
-            case 'unauthenticated':
-                $status  = 401;
-                $message ?? 'User token has been expired';
-            break;
-
-            case 'notfound':
-                $status  = 404;
-                $message ?? 'Sorry no results query for your request';
-            break;
-
-            case 'forbidden':
-                $status  = 403;
-                $message ??  'You don\'t have permission to access this content';
-            break;
-
-            default:
-                $status = 500;
-                $message ?? $message = 'Server error, please try again later';
-            break;
-        }
-
-        $response = [
-            'status'    =>  $status,
-            'message'   =>  $message,
-            'data'      =>  $data
-        ];
-
-        return response()->json($response,$status);
-    }
-}
 
 ?>
